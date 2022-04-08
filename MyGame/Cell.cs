57d79990de7569dc373baaf4b7745abca268e5ca
Артем Воловий <artem.volovyi@nure.ua>
2022-04-08 22:25:58 +0300

    Project started

---
+++ MyGame/Cell.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyGame
+{
+    public struct Cell
+    {
+        public enum CellTypes
+        {
+            Player = 'I',
+            Prize = '@',
+            Stop = '.',
+            Wall = '#',
+            Trap = '%'
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public CellTypes CellType { get; }
+
+        public ConsoleColor Color { get; }
+
+        public Cell(int x, int y, string type, ConsoleColor color)
+        {
+            X = x;
+            Y = y;
+            CellType = (CellTypes)Enum.Parse(typeof(MyGame.Cell.CellTypes), type);
+            Color = color;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write((char)CellType);
+        }
+
+        public void Clear()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(" ");
+        }
+    }
+}
--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿namespace MyGame
+{
+    class Program
+    {
+        public const int MapWidth = 20;
+        public const int MapHeight = 10;
+        public const ConsoleColor BorderColor = ConsoleColor.Red;
+
+        static void Main()
+        {
+            Console.SetWindowSize(MapWidth, MapHeight);
+            Console.SetBufferSize(MapWidth, MapHeight);
+            Console.CursorVisible = false;
+            
+            DrawBorder();
+            Console.ReadKey();
+        }
+
+        static void DrawBorder()
+        {
+            for (int i = 0; i < MapWidth; i++)
+            {
+                new Cell(i, 0, "Wall", BorderColor).Draw();
+                new Cell(i, MapHeight - 1, "Wall", BorderColor).Draw();
+            }
+
+            for (int i = 0; i < MapHeight; i++)
+            {
+                new Cell(0, i, "Wall", BorderColor).Draw();
+                new Cell(MapWidth - 1, i, "Wall", BorderColor).Draw();
+            }
+        }
+    }
+}
